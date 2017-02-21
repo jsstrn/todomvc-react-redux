a834afd4de0ff40ffebbc99697d7afaa099fde83
Jesstern Rays <jessternrays@gmail.com>
2017-02-21 17:18:55 +0800

    Add starter files

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const config = {
+  entry: './index.js'
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: 'babel-loader'}
+    ]
+  }
+}
+
+module.exports = config
